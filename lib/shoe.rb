c32149a1d9ed25f74ac88e08e75a99a8d7801320
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-16 00:44:46 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+
   end
 
   def cobble
@@ -15,7 +16,8 @@ class Shoe
   
   def brand=(brand)
     @brand = brand
-    BRANDS << brand 
+     if !(BRANDS.include?(brand))
+       BRANDS << brand
   end
   
   
